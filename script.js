57cc3db4466c24db22b327bd25feb1fcb86e4101
Gal <galhamo114@gmail.com>
2024-11-24 16:01:58 +0200

    removed the part that is responsible for the for loop because i didnt succeed to manage that now everything works just fine except the game doesnt stop after 5 rounds

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const MAX_ROUNDS = 5
+
 const choiceTable = ['rock', 'paper', 'scissors']
+let roundNum = 0
 
 const rockButt = document.createElement('button')
 rockButt.innerHTML = 'Rock'
@@ -19,65 +21,74 @@ const getComputerChoice = () => {
     return choiceTable.at(randomNum)
 }
 
+let humanScore = 0
+let computerScore = 0
 const playGame = (buttEvent) => {
-    let humanScore = 0
-    let computerScore = 0
-
+    
     const rules = {
         scissors : 'paper',
         paper : 'rock',
         rock : 'scissors'
     }
-
-    const playRound = (userChoice) => {
-        let computerChoice = getComputerChoice()
-
+    
+    const playRound = (userChoice, computerChoice) => {
+        console.log(userChoice, computerChoice)
         if (userChoice === computerChoice){
             gameResults.innerHTML = 'Draw'
         } else if (rules[userChoice] === computerChoice) {
-            gameResults.innerHTML = 'Human wins'
+            gameResults.innerHTML = 'You won that round'
             humanScore++
         } else {
-            gameResults.innerHTML = 'You lose!'
+            gameResults.innerHTML = 'You lost that round'
             computerScore++
+        }
+        roundNum++
+
+        if(roundNum === 5){
+            gameResults.innerHTML = `Human score: ${humanScore} || Computer score: ${computerScore}`
+
+            if (humanScore > computerScore){
+                gameResults.innerHTML += ` Human Wins!`
+            } else {
+                gameResults.innerHTML += ` Computer Wins!`
+            }
+            
         }
     }
 
     if (buttEvent === 'Start'){
-        for (let roundNum = 0; roundNum < MAX_ROUNDS; roundNum++) {
+        rockButt.addEventListener('click', (e) => {
+            const rockChoice = 'rock'
+            const computerChoice = getComputerChoice()
+            playRound(rockChoice, computerChoice)
+        })
+        paperButt.addEventListener('click', (e) => {
+            const paperChoice = 'paper'
+            const computerChoice = getComputerChoice()
+            playRound(paperChoice, computerChoice)
+        })
+        scissorsButt.addEventListener('click', (e) => {
+            const scissorsChoice = 'scissors'
+            const computerChoice = getComputerChoice()
+            playRound(scissorsChoice, computerChoice)
+        })
 
-            rockButt.addEventListener('click', (e) => {
-                const rockChoice = 'rock'
-                playRound(rockChoice)
-            })
-            paperButt.addEventListener('click', (e) => {
-                const paperChoice = 'paper'
-                playRound(paperChoice)
-            })
-            scissorsButt.addEventListener('click', (e) => {
-                const scissorsChoice = 'scissors'
-                playRound(scissorsChoice)
-            })
-        }
-        gameResults.innerHTML = `Human score: ${humanScore} || Computer score: ${computerScore}`
-
-        if (humanScore > computerScore){
-            gameResults.innerHTML += `Human Wins!`
-        } else {
-            gameResults.innerHTML += `You Lose!`
-        }
     }
+    
 
 }
-
+document.body.appendChild(gameResults)
 document.body.appendChild(startButt)
 
 startButt.addEventListener('click', () => {
     document.body.removeChild(startButt)
     let startGame = 'Start'
-    playGame(startGame)
 
     document.body.appendChild(rockButt)
     document.body.appendChild(paperButt)
     document.body.appendChild(scissorsButt)
+
+    playGame(startGame)
 })
+
+
